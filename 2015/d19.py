0c8e2aa50dae82aa42f04833bc8588e878a73d44
Kenton Lam <kenton_lam@outlook.com>
2021-12-03 00:09:49 +1000

    working.

--- 2015/d19.py
+++ 2015/d19.py
@@ -19,7 +19,7 @@ def parse(data: str) -> Data:
             mol = l
     return d, mol
 
-def solve1(data: Data):
+def solve1(data: Data) -> set[str]:
     repls, mol = data
     x: set[str] = set()
     for old, news in repls.items():
@@ -28,7 +28,8 @@ def solve1(data: Data):
             while i >= 0:
                 x.add(mol[:i] + new + mol[i+len(old):])
                 i = mol.find(old, i+1)
-    print(len(x))
+    # print(len(x))
+    return x
 
 T = TypeVar('T')
 
@@ -46,6 +47,17 @@ def find_terminal(data: Data, n: int) -> set[str]:
 
     return produced - nonterm
 
+def subseq(a: list[str], b: list[str], ai: int = 0, bi: int = 0) -> bool:
+    ai = bi = 0
+    while ai < len(a) and bi < len(b):
+        if a[ai] == b[bi]:
+            ai += 1
+            bi += 1
+        else:
+            bi += 1
+    return ai == len(a)
+
+
 def solve2(data: Data):
     repls, mol = data
     term1 = find_terminal(data, 1)
@@ -53,9 +65,27 @@ def solve2(data: Data):
     pat1 = re.compile('('+'|'.join(term1)+')')
     print(pat1.sub(r'(\1)', mol))
 
-    nonterm_repls: dict[str,set[str]] = defaultdict(set)
+    def trace(s: str):
+        return pat1.findall(s)
 
+    INF = 1 << 31
+    trace_mol = trace(mol)
+    def go(m: str) -> int:
+        if len(m) > len(mol): return INF
+        t = trace(m)
+        if not subseq(t, trace_mol): return INF
+        if len(m) == len(mol) and m == mol: return 0
 
+        min_steps = INF
+        for m2 in solve1((repls, m)):
+            g2 = go(m2)
+            if g2 < min_steps:
+                min_steps = g2
+        return min_steps + 1
+
+    print(trace(mol))
+    print(go(mol))
+    print(go('CRnCaCaCaSiRnBPTiMgArSiRnSiRnMgArSiRnCaFArTiTiBSiThFYCaFArCaCaSiThCaPBSiThSiThCaCaPTiRnPBSiThRnFArArCaCaSiThCaSiThSiRnMgArCaPTiBPRnFArSiThCaSiRnFArBCaSiRnCaPRnFArPMgYCaFArCaPTiTiTiBPBSiThCaPTiBPBSiRnFArBPBSiRnCaFArBPRnSiRnFArRnSiRnMgArCaFArCaCaCaSiThSiThCaCaPBPTiTiRnFArCaPTiBSiAlArPBCaCaCaCaCaSiRnMgArCaSiThFArThCaSiThCaSiRnCaFYCaSiRnFYFArFArCaSiRnFYFArCaSiRnBPMgArSiThPRnFArCaSiRnFArTiRnSiRnFYFArCaSiRnMgArCaSiRnTiMgArSiThCaSiThCaFArPRnFArSiRnFArTiTiTiTiBCaCaSiRnCaCaFYFArSiThCaPTiBPTiBCaSiThSiRnMgArCaF'))
 
 if __name__ == '__main__':
     input = parse(open('19.txt').read())

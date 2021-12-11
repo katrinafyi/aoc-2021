5f7b0e30911790799638db1b5a089224bf5a5157
Kenton Lam <kenton_lam@outlook.com>
2021-12-12 00:45:11 +1000

    Day 12.

--- aoc/app/Day12.hs
+++ aoc/app/Day12.hs
@@ -27,37 +27,61 @@ parse raw = foldl (\m ft -> M.insertWith S.union (ft!!0) (S.singleton $ ft!!1) m
 isBig :: [Char] -> Bool
 isBig = all isUpper . take 1
 
+isSmall :: [Char] -> Bool
+isSmall = not . isBig
+
 cartesian2 :: [a] -> [b] -> [(a,b)]
 cartesian2 = liftA2 (,)
 
-connect :: M.Map String (S.Set String) -> String -> M.Map String (S.Set String)
-connect m k = M.unionsWith (<>) [withoutBig, big, big2]
-    where
-        withoutBig = M.withoutKeys (fmap (S.filter (not . isBig)) m) outgoing
-        incoming = fmap fst $ filter (S.member k . snd) $ M.toList m
-        outgoing = M.findWithDefault S.empty k m
-        big = M.fromListWith S.union $ second S.singleton <$> cartesian2 incoming (S.toList outgoing)
-        big2 = M.fromListWith S.union $ second S.singleton <$> cartesian2 (S.toList outgoing) incoming
+-- connect :: M.Map String (S.Set String) -> String -> M.Map String (S.Set String)
+-- connect m k = M.unionsWith (<>) [withoutBig, big, big2]
+--     where
+--         withoutBig = M.withoutKeys (fmap (S.filter (not . isBig)) m) outgoing
+--         incoming = fmap fst $ filter (S.member k . snd) $ M.toList m
+--         outgoing = M.findWithDefault S.empty k m
+--         big = M.fromListWith S.union $ second S.singleton <$> cartesian2 incoming (S.toList outgoing)
+--         big2 = M.fromListWith S.union $ second S.singleton <$> cartesian2 (S.toList outgoing) incoming
 
-normalise :: M.Map String (S.Set String) -> M.Map String (S.Set String)
-normalise m = foldl connect m bigs
-    where bigs = filter (all isUpper) $ M.keys m
-
+-- normalise :: M.Map String (S.Set String) -> M.Map String (S.Set String)
+-- normalise m = foldl connect m bigs
+--     where bigs = filter (all isUpper) $ M.keys m
 
 allPaths :: M.Map String (S.Set String) -> [[String]]
-allPaths m = go m S.empty "start"
+allPaths m = go S.empty "start"
     where
-        go :: M.Map String (S.Set String) -> S.Set String -> String -> [[String]]
-        go m s = undefined
+        go :: S.Set String -> String -> [[String]]
+        go _ "end" = [["end"]]
+        go s x = do
+            guard $ isBig x || x `S.notMember` s
+            x' <- maybe [] S.toList $ m M.!? x
+            (x:) <$> go (S.insert x s) x'
+
+allPaths2 :: M.Map String (S.Set String) -> [[String]]
+allPaths2 m = go S.empty Nothing "start"
+    where
+        go :: S.Set String -> Maybe String -> String -> [[String]]
+        go _ _ "end" = [["end"]]
+        go s t x = do
+            guard $ t /= Just "start"
+            guard $ isBig x || x `S.notMember` s || isNothing t
+            let t' = if isSmall x && x `S.member` s then Just x else t
+            x' <- maybe [] S.toList $ m M.!? x
+            (x:) <$> go (S.insert x s) t' x'
+
+solve1 :: M.Map String (S.Set String) -> Int
+solve1 = length . allPaths
+
+solve2 :: M.Map String (S.Set String) -> Int
+solve2 = length . allPaths2
 
 main' :: String -> IO ()
 main' raw = do
     let input = parse raw
     print $ input
-    print $ normalise $ input
+    -- mapM_ putStrLn $ intercalate "," <$> allPaths2 input
 
-    -- print $ solve1 input
-    -- print $ solve2 input
+    print $ solve1 input
+    print $ solve2 input
 
 run :: IO ()
 run = readFile "in/12.txt" >>= main'
